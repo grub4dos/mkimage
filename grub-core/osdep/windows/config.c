1f67ad014b67abb5370064fccbea11055cb5c98b
a1ive <10670106+a1ive@users.noreply.github.com>
2021-06-02 11:36:00 +0800

    add win32 func

--- grub-core/osdep/windows/config.c
+++ grub-core/osdep/windows/config.c
@@ -41,6 +41,116 @@
 #include <winioctl.h>
 #include <wincrypt.h>
 
+
+#ifdef __CYGWIN__
+#include <sys/cygwin.h>
+#endif
+
+#if SIZEOF_TCHAR == 1
+
+LPTSTR
+grub_util_utf8_to_tchar (const char *in)
+{
+  return xstrdup (in);
+}
+
+char *
+grub_util_tchar_to_utf8 (LPCTSTR in)
+{
+  return xstrdup (in);
+}
+
+#elif SIZEOF_TCHAR == 2
+
+LPTSTR
+grub_util_utf8_to_tchar (const char *in)
+{
+  LPTSTR ret;
+  size_t ssz = strlen (in);
+  size_t tsz = 2 * (GRUB_MAX_UTF16_PER_UTF8 * ssz + 1);
+  ret = xmalloc (tsz);
+  tsz = grub_utf8_to_utf16 (ret, tsz,
+                  (const grub_uint8_t *) in, ssz, NULL);
+  ret[tsz] = 0;
+  return ret;
+}
+
+char *
+grub_util_tchar_to_utf8 (LPCTSTR in)
+{
+  size_t ssz;
+  for (ssz = 0; in[ssz]; ssz++);
+
+  size_t tsz = GRUB_MAX_UTF8_PER_UTF16 * ssz + 1;
+  grub_uint8_t *ret = xmalloc (tsz);
+  *grub_utf16_to_utf8 (ret, in, ssz) = '\0';
+  return (char *) ret;
+}
+
+#else
+#error "Unsupported TCHAR size"
+#endif
+
+
+static LPTSTR
+grub_util_get_windows_path_real (const char *path)
+{
+  LPTSTR fpa;
+  LPTSTR tpath;
+  size_t alloc, len;
+
+  tpath = grub_util_utf8_to_tchar (path);
+
+  alloc = PATH_MAX;
+
+  while (1)
+  {
+    fpa = xcalloc (alloc, sizeof (fpa[0]));
+    len = GetFullPathName (tpath, alloc, fpa, NULL);
+    if (len >= alloc)
+    {
+      free (fpa);
+      alloc = 2 * (len + 2);
+      continue;
+    }
+    if (len == 0)
+    {
+      free (fpa);
+      return tpath;
+    }
+    free (tpath);
+    return fpa;
+  }
+}
+
+#ifdef __CYGWIN__
+LPTSTR
+grub_util_get_windows_path (const char *path)
+{
+  LPTSTR winpath;
+  /* Workaround cygwin bugs with //?/.  */
+  if ((path[0] == '\\' || path[0] == '/')
+      && (path[1] == '\\' || path[1] == '/')
+      && (path[2] == '?' || path[2] == '.')
+      && (path[3] == '\\' || path[3] == '/'))
+    return grub_util_get_windows_path_real (path);
+
+  winpath = xmalloc (sizeof (winpath[0]) * PATH_MAX);
+  memset (winpath, 0, sizeof (winpath[0]) * PATH_MAX);
+  if (cygwin_conv_path ((sizeof (winpath[0]) == 1 ? CCP_POSIX_TO_WIN_A
+              : CCP_POSIX_TO_WIN_W) | CCP_ABSOLUTE, path, winpath,
+              sizeof (winpath[0]) * PATH_MAX))
+    grub_util_error ("%s", _("cygwin_conv_path() failed"));
+  return winpath;
+}
+#else
+LPTSTR
+grub_util_get_windows_path (const char *path)
+{
+  return grub_util_get_windows_path_real (path);
+}
+#endif
+
 static int allow_fd_syncs = 1;
 
 #ifdef __MINGW32__
